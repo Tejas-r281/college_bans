c22cedb62e1df6a813a705c150dd1b4c5aedf88d
Raushan <raushan.043.kumar@gmail.com>
2022-01-06 01:33:03 +0530

    first commit

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>College Project</h1>
+      <h2>A simple React app</h2>
+    </div>
+  );
+}
+// voice nahi aa raha 
+
+export default Header;
